33747c14ce6593ba1135b3091dfa9d46ba294275
boertel <ben@comediadesign.com>
2024-02-22 10:09:46 -0800

    try better font

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,51 +1,50 @@
 <script>
-    export let contact;
+	export let contact;
 </script>
 
 <header>
-    <div />
-    <div class="contact">
-        <div class="avatar" />
-        <h1>{contact}</h1>
-    </div>
-    <div />
+	<div />
+	<div class="contact">
+		<div class="avatar" />
+		<h1>{contact}</h1>
+	</div>
+	<div />
 </header>
 
 <style>
-    header {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding: 1rem;
-        backdrop-filter: blur(var(--blur));
-        position: fixed;
-        top: 0;
-        right: 0;
-        left: 0;
-        height: 60px;
-        box-sizing: border-box;
-        padding: 0;
-        z-index: 1;
-    }
-    header:before {
-        content: '';
-        position: absolute;
-        inset: 0;
-        z-index: -1;
-        background-color: rgba(255, 255, 255, 0.06);
-    }
+	header {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 1rem;
+		backdrop-filter: blur(var(--blur));
+		position: fixed;
+		top: 0;
+		right: 0;
+		left: 0;
+		height: 60px;
+		box-sizing: border-box;
+		padding: 0;
+		z-index: 1;
+	}
+	header:before {
+		content: '';
+		position: absolute;
+		inset: 0;
+		z-index: -1;
+		background-color: rgba(255, 255, 255, 0.06);
+	}
 
-    .contact {
-        display: flex;
-        align-items: center;
-    }
+	.contact {
+		display: flex;
+		align-items: center;
+	}
 
-    h1 {
-        font-family: -apple-system;
-        margin: 0;
-        padding: 0;
-        font-size: 1em;
-        font-weight: normal;
-        color: rgb(var(--foreground-1));
-    }
+	h1 {
+		margin: 0;
+		padding: 0;
+		font-size: 1em;
+		font-weight: normal;
+		color: rgb(var(--foreground-1));
+	}
 </style>
--- src/lib/Message.svelte
+++ src/lib/Message.svelte
@@ -1,114 +1,127 @@
 <script>
-    export let text;
-    export let contact;
-    export let delay = 0;
+	export let text;
+	export let contact;
+	export let delay = 0;
 </script>
 
-<li class="{contact}">{#if delay !== 0}<p class="typing"><span /><span /><span /></p>{:else}<p>{@html text}</p>{/if}</li>
+<li class={contact}>
+	{#if delay !== 0}<p class="typing"><span /><span /><span /></p>{:else}<p>{@html text}</p>{/if}
+</li>
 
 <style>
-    @keyframes dots {
-        0% { opacity: 1; } 
-    }
-    li {
-        display: flex;
-        font-family: -apple-system;
-        margin: 0;
-        padding: 0;
-    }
-    li.other {
-        justify-content: flex-start;
-        margin-right: 20px;
-    }
-    li.user {
-        justify-content: flex-end;
-        margin-left: 20px;
-    }
+	@keyframes dots {
+		0% {
+			opacity: 1;
+		}
+	}
+	li {
+		display: flex;
+		margin: 0;
+		padding: 0;
+	}
+	li.other {
+		justify-content: flex-start;
+		margin-right: 20px;
+	}
+	li.user {
+		justify-content: flex-end;
+		margin-left: 20px;
+	}
 
-    li p {
-        position: relative;
-        display: inline-flex;
-        list-style: none;
-        padding: 12px 18px;
-        margin: 0;
-        min-height: 43px;
-    }
+	li p {
+		position: relative;
+		display: inline-flex;
+		list-style: none;
+		padding: 12px 18px;
+		margin: 0;
+		min-height: 43px;
+	}
 
-    :global(li p img) {
-        width: 50%;
-    }
+	:global(li p img) {
+		width: 50%;
+	}
 
-    li p.typing {
-        gap: 4px;
-        align-items: center;
-    }
+	li p.typing {
+		gap: 4px;
+		align-items: center;
+	}
 
-    li p.typing span {
-        border-radius: 100%;
-        width: 12px;
-        height: 12px;
-        background: rgb(var(--foreground-1));
-        opacity: 0.3;
-        animation: dots .9s ease-in-out infinite;
-    }
+	li p.typing span {
+		border-radius: 100%;
+		width: 12px;
+		height: 12px;
+		background: rgb(var(--foreground-1));
+		opacity: 0.3;
+		animation: dots 0.9s ease-in-out infinite;
+	}
 
-    li p.typing span:nth-child(1) { animation-delay: 0s; }
-    li p.typing span:nth-child(2) { animation-delay: .3s; }
-    li p.typing span:nth-child(3) { animation-delay: .6s; }
+	li p.typing span:nth-child(1) {
+		animation-delay: 0s;
+	}
+	li p.typing span:nth-child(2) {
+		animation-delay: 0.3s;
+	}
+	li p.typing span:nth-child(3) {
+		animation-delay: 0.6s;
+	}
 
-    li p:before {
-        content: "";
-        position: absolute;
-        height: 20px;
-        width: 20px;
-        bottom: 0;
-        z-index: 0;
-    }
-    li p:after {
-        content: "";
-        position: absolute;
-        z-index: 1;
-        bottom: 0;
-        width: 10px;
-        height: 20px;
-        background: rgb(var(--background));
-    }
+	li p:before {
+		content: '';
+		position: absolute;
+		height: 20px;
+		width: 20px;
+		bottom: 0;
+		z-index: 0;
+	}
+	li p:after {
+		content: '';
+		position: absolute;
+		z-index: 1;
+		bottom: 0;
+		width: 10px;
+		height: 20px;
+		background: rgb(var(--background));
+	}
 
-    li.user p {
-        border-radius: 20px 20px 10px 20px;
-        background-color: rgb(var(--primary));
-        color: rgb(var(--foreground));
-    }
-    li.user p:before {
-        right: -8px;
-        background: rgb(var(--primary));
-    }
-    li.user:first-child p:before, li.other + li.user p:before {
-        border-bottom-left-radius: 15px;
-    }
-    li.user p:after {
-        right: -10px;
-    }
-    li.user:first-child p:after, li.other + li.user p:after {
-        border-bottom-left-radius: 10px;
-    }
+	li.user p {
+		border-radius: 20px 20px 10px 20px;
+		background-color: rgb(var(--primary));
+		color: rgb(var(--foreground));
+	}
+	li.user p:before {
+		right: -8px;
+		background: rgb(var(--primary));
+	}
+	li.user:first-child p:before,
+	li.other + li.user p:before {
+		border-bottom-left-radius: 15px;
+	}
+	li.user p:after {
+		right: -10px;
+	}
+	li.user:first-child p:after,
+	li.other + li.user p:after {
+		border-bottom-left-radius: 10px;
+	}
 
-    li.other p {
-        border-radius: 20px 20px 20px 10px;
-        background-color: rgb(var(--background-1));
-        color: rgb(var(--foreground-1));
-    }
-    li.other p:before {
-        left: -7px;
-        background: rgb(var(--background-1));
-    }
-    li.other:first-child p:before, li.user + li.other p:before {
-        border-bottom-right-radius: 15px;
-    }
-    li.other p:after {
-        left: -10px;
-    }
-    li.other:first-child p:after, li.user + li.other p:after {
-        border-bottom-right-radius: 10px;
-    }
+	li.other p {
+		border-radius: 20px 20px 20px 10px;
+		background-color: rgb(var(--background-1));
+		color: rgb(var(--foreground-1));
+	}
+	li.other p:before {
+		left: -7px;
+		background: rgb(var(--background-1));
+	}
+	li.other:first-child p:before,
+	li.user + li.other p:before {
+		border-bottom-right-radius: 15px;
+	}
+	li.other p:after {
+		left: -10px;
+	}
+	li.other:first-child p:after,
+	li.user + li.other p:after {
+		border-bottom-right-radius: 10px;
+	}
 </style>
